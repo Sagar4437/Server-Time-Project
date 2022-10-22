76461dde78efcd8901f0faa2d441dd75d6409b8f
Sagar Darade <daradesagar.1998@gmail.com>
2022-10-22 20:22:14 +0530

    added static css files

--- ServerTime/settings.py
+++ ServerTime/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC = Path(BASE_DIR,'static')
+TEMPLATES_DIR = Path(BASE_DIR,'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'ServerTime.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- timeApp/views.py
+++ timeApp/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def giveTime(request):
     from datetime import datetime
     time = datetime.now().strftime("%H : %M : %S")
-    value = f"Hellow Guest... Current Server time is {time}"
-    return HttpResponse(value)
+    return render(request,'timeApp/index.html',{'time':time})
